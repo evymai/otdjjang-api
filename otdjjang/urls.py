6ffffa19fb92f9ec6987572ad76216259bd0c3c1
Emily Mai <emily.vy.mai@gmail.com>
2024-05-09 17:14:19 -0500

    bug fixes for mvp

--- otdjjang/urls.py
+++ otdjjang/urls.py
@@ -5,7 +5,7 @@ from rest_framework import routers
 from rest_framework.authtoken.views import obtain_auth_token
 from otdjjangapi.models import *
 from otdjjangapi.views import *
-
+from django.conf.urls.static import static
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r"brands", Brands, "brand")
@@ -15,6 +15,7 @@ router.register(r"articles", Articles, "article")
 router.register(r"userarticles", UserArticles, "userarticle")
 router.register(r"outfits", Outfits, "outfit")
 router.register(r"outfitarticles", OutfitArticles, "outfitarticle")
+router.register(r"outfitphotos", OutfitPhotos, "outfitphoto")
 
 
 urlpatterns = [
@@ -22,5 +23,5 @@ urlpatterns = [
     path("register", register_user),
     path("login", login_user),
     path("api-token-auth", obtain_auth_token),
-    path("api-auth", include("rest_framework.urls", namespace="rest_framework"))
-]
+    path("api-auth", include("rest_framework.urls", namespace="rest_framework")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- otdjjangapi/models/outfitphoto.py
+++ otdjjangapi/models/outfitphoto.py
@@ -1,8 +1,7 @@
 from django.db import models
-from .outfit import Outfit
 
 
 class OutfitPhoto(models.Model):
     outfit = models.ForeignKey("Outfit", on_delete=models.SET_NULL, null=True)
     worn_on = models.DateTimeField()
-    image_url = models.URLField(null=True)
+    image = models.ImageField(upload_to='fitcheck_images/')
